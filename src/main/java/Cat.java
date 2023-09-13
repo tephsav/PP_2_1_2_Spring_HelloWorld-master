7709e71ec5011e059e89ee3ba7ce7c3aa39bd4ed
Your Name <you@example.com>
2023-09-13 12:07:42 +0300

    Added POJO requirements for the Cat class

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,45 @@
+import java.util.Objects;
+
 public class Cat {
+    private Long id;
+    private String name;
+
+    public Cat() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(id, cat.id) && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
